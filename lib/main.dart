4da23ed4b5e3b2d6655ec3367ed54197a98b74a9
Mai Thành Duy An <tiachop0102@gmail.com>
2020-02-10 01:06:12 +0700

    UI Design - Remove main_app.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:sme_mobile/screens/main_app.dart';
+import 'package:sme_mobile/routes/routes.dart';
+import 'package:sme_mobile/screens/panel.dart';
 
 void main(){
   {
@@ -12,6 +13,28 @@ void main(){
 
     runApp(
         MainApp()
+    );
+  }
+}
+
+class MainApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SME',
+      theme: ThemeData(
+        primaryColor: Colors.blue
+      ),
+      //localizationsDelegates:,
+      supportedLocales: [
+        const Locale('en'), // English,
+        const Locale('vi'), // English,
+      ],
+      home: PanelControl(
+        appName: 'POS',
+      ),
+      routes: Routes.initialAppRoute,
+      debugShowCheckedModeBanner: false,
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:sme_mobile/main.dart';
 
-import 'package:sme_mobile/screens/main_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
